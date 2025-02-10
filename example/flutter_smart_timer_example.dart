f96cc997cc5e6853c43ad4fbe7d2e84b697e7270
Ainur <69866986+Anykeykin@users.noreply.github.com>
2025-02-10 21:05:45 +0300

    use custom direction on timer

--- example/flutter_smart_timer_example.dart
+++ example/flutter_smart_timer_example.dart
@@ -2,6 +2,7 @@ import 'package:flutter_smart_timer/flutter_smart_timer.dart';
 
 void main() {
   final timer = SmartTimer(
+    customDuration: 5,
     intervals: [5, 10, 15],
     onTick: (int time) {
       print('tick time $time');
